952e7806a6812ac79fd3107d18420fba1cdcc7fa
dkibalnikov <Dmitrii_Kibalnikov@eprupetw11b5.home>
2023-05-07 10:55:13 +0300

    fix data table dot "."

--- R/run_bg.R
+++ R/run_bg.R
@@ -77,6 +77,7 @@ run_view <- function(cntx=NULL){
     context <- cntx
   }
   # View result of selected run
+  require(data.table, include.only = .__C__data.table) # fix data table dot "."
   View(eval(str2expression(paste0("{", context, "}"))), title = 'jobber')
 
 }
